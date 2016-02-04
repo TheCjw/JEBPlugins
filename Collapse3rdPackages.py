edb75cd2fe81f299a843b4ccc4472c075ecef220
TheCjw <thecjw@live.com>
2016-02-04 17:00:13 +0800

    Remove Pattern reflection.

--- Collapse3rdPackages.py
+++ Collapse3rdPackages.py
@@ -5,6 +5,8 @@
 
 __author__ = "TheCjw"
 
+from java.util.regex import Pattern
+
 from jeb.api import IScript
 from jeb.api.ui import View
 
@@ -46,37 +48,21 @@ class Collapse3rdPackages(IScript):
         instance_real_view = field_real_view.get(instance_tree_view)
 
         field_pattern = None
-        class_pattern = None
         for field in instance_real_view.getClass().getDeclaredFields():
             if field.getType().getName() == "java.util.regex.Pattern":
                 field_pattern = field
-                class_pattern = field.getType()
                 break
 
-        if field_pattern is None or class_pattern is None:
-            print "java.util.regex.Pattern field is not found."
+        if field_pattern is None:
+            print "Pattern field is not found."
             return
 
         field_pattern.setAccessible(True)
 
-        method_compile = None
-        for method in class_pattern.getMethods():
-            if method.getName() == "compile":
-                args = 0
-                for x in method.getParameterTypes():
-                    args += 1
-                if args == 1:
-                    method_compile = method
-                    break
-
-        if method_compile is None:
-            print "java.util.regex.Pattern#compile is not found."
-            return
-
         temp = "|".join(third_party_packages.strip().replace(".", "\.").splitlines())
         temp = "^({0}).*".format(temp)
         print temp
-        new_pattern = method_compile.invoke(None, temp)
+        new_pattern = Pattern.compile(temp)
 
         field_pattern.set(instance_real_view, new_pattern)
 
